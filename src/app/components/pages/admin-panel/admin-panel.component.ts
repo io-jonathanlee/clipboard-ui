2e684201869d0b91cd3eaa20b051c921fd0c6a27
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-01-19 17:30:46 +0000

    Removed duplicate asset files

--- src/app/components/pages/admin-panel/admin-panel.component.ts
+++ src/app/components/pages/admin-panel/admin-panel.component.ts
@@ -4,16 +4,18 @@ import {Component} from '@angular/core';
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
   styleUrls: ['./admin-panel.component.css',
-    './assets/css/style.css',
-    './assets/vendor/bootstrap-icons/bootstrap-icons.css',
-    './assets/vendor/bootstrap/css/bootstrap.min.css',
-    './assets/vendor/boxicons/css/boxicons.min.css',
-    './assets/vendor/quill/quill.bubble.css',
-    './assets/vendor/quill/quill.snow.css',
-    './assets/vendor/remixicon/remixicon.css',
-    './assets/vendor/remixicon/remixicon.css',
-    './assets/vendor/simple-datatables/style.css'],
+    'assets/vendor/bootstrap-icons/bootstrap-icons.css',
+    'assets/vendor/bootstrap/css/bootstrap.min.css',
+    'assets/vendor/boxicons/css/boxicons.min.css',
+    'assets/vendor/quill/quill.bubble.css',
+    'assets/vendor/quill/quill.snow.css',
+    'assets/vendor/remixicon/remixicon.css',
+    'assets/vendor/remixicon/remixicon.css',
+    'assets/vendor/simple-datatables/style.css'],
 })
+/**
+ * Admin panel component.
+ */
 export class AdminPanelComponent {
 
 }
