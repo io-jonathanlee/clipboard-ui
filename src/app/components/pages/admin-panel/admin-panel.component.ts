aa1fa225338194f284533a2b4038f69f19ff7b6d
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-01-20 01:04:23 +0000

    Updated budgets in angular.json

--- src/app/components/pages/admin-panel/admin-panel.component.ts
+++ src/app/components/pages/admin-panel/admin-panel.component.ts
@@ -3,15 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.css',
-    'assets/vendor/bootstrap-icons/bootstrap-icons.css',
-    'assets/vendor/bootstrap/css/bootstrap.min.css',
-    'assets/vendor/boxicons/css/boxicons.min.css',
-    'assets/vendor/quill/quill.bubble.css',
-    'assets/vendor/quill/quill.snow.css',
-    'assets/vendor/remixicon/remixicon.css',
-    'assets/vendor/remixicon/remixicon.css',
-    'assets/vendor/simple-datatables/style.css'],
+  styleUrls: ['./admin-panel.component.css'],
 })
 /**
  * Admin panel component.
